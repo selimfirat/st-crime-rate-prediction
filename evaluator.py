c6f5c4ef2ce94c16bb629ab4a7486bc80428172f
selimfirat <yilmazselimfirat@gmail.com>
2019-05-23 16:44:51 +0000

    gcn added and r2 scorer implemented

--- evaluator.py
+++ evaluator.py
@@ -2,25 +2,9 @@ import numpy as np
 
 class Evaluator:
     
-    def calculate_accuracy(self, y, y_pred):
-        
-        return 1.0* np.sum(y.flatten() == np.around(y_pred.flatten())) / (33*len(y))
-    
-    def calculate_macro_f1(self, y, y_pred):
-        
-        
-        f1s= 0.0
-        for i in range(33):
-            
-            topc = (y_pred[:, i] == 1)
-            
-            top = np.sum(np.logical_and(topc, y[:, i] == 1))
-            
-            precision = 1.0*top/topc
-            recall = 1.0*top/np.sum(y[:, i] == 1)
-            
-            f1 = 2/(1.0/recall + 1.0/precision)
-            
-            f1s += f1
-    
-        return f1s/33
+    def score_r2(self, y_true, y_pred, multioutput="uniform_average"):
+        new_shape = (y_true.shape[0]*y_true.shape[1], y_true.shape[2])
+        y_true = y_true.reshape(new_shape)
+        y_pred = y_pred.reshape(new_shape)
+
+        return r2_score(y_true, y_pred, multioutput=multioutput)
